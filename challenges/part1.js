79ee89fa5c6d9b229b4b3c2dcd2f193f07082f51
bacarlsson86 <bacarlsson@gmail.com>
2023-08-10 22:21:59 -0700

    completing part1

--- challenges/part1.js
+++ challenges/part1.js
@@ -1,1 +1,14 @@
 /** Write your code below */
+let post = ""
+
+jsInput = document.querySelector('input')
+jsDiv = document.querySelector('div')
+
+jsDiv.textContent = post;
+
+const handleInput = () => {
+  post = jsInput.value;
+  jsDiv.textContent = post;
+}
+
+jsInput.oninput = handleInput;
