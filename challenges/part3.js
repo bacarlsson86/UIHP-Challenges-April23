4191e7be2c92da88698c680b1b21b1f255e58657
bacarlsson86 <bacarlsson@gmail.com>
2023-08-20 16:58:03 -0700

    completed part3

--- challenges/part3.js
+++ challenges/part3.js
@@ -1,26 +1,43 @@
 let myName = '';
 let isFocus = false;
+let jsInput;
+let jsDiv;
+let vDOM;
 
-let jsInput = document.querySelector('input');
-let jsDiv = document.querySelector('div');
-
-function dataToView() {
-  jsInput.value = myName;
-  jsDiv.textContent = myName;
+function createVDOM () {
+  return [[
+    'input',
+    myName,
+    function handle() {
+      myName = jsInput.value;
+    },
+  ],['div', `Hello, ${myName}!`]];
 }
 
-function handleInput() {
-  myName = jsInput.value;
+function convert (node) {
+  const elem = document.createElement(node[0]);
+  // console.log(node);
+  if (node[0] === 'div') {
+    jsDiv = elem;
+    elem.textContent = node[1];
+  } else if (node[0] === 'input') {
+    jsInput = elem;
+    elem.value = node[1];
+  }
+  if (node[2]) elem.oninput = node[2];
+  // console.log(elem);
+  return elem;
 }
 
-jsInput.oninput = handleInput;
+function updateDOM () {
+  // console.log('updating')
+  document.activeElement === jsInput ? (isFocus = true) : (isFocus = false);
+  vDOM = createVDOM();
+  // console.log(vDOM);
+  const elems = vDOM.map(node => convert(node));
+  document.body.replaceChildren(...elems);
+  if (isFocus) jsInput.focus();
 
-// function component() {
-//   document.activeElement === jsInput ? (isFocus = true) : (isFocus = false);
+}
 
-//   //your code here
-
-//   if (isFocus) jsInput.focus();
-// }
-
-setInterval(dataToView, 15);
+setInterval(updateDOM, 15)
